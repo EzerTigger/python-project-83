77e7a1117800811a6f7f32ba1102b6c9d990d114
Sergei Ezerskii <gaftezer@gmail.com>
2023-07-26 15:21:25 +0300

    Step 5

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,12 +4,13 @@ import psycopg2
 import os
 import datetime
 import validators
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.urandom(12).hex()
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL_DEV')
 
 
 @app.route('/')
@@ -46,7 +47,7 @@ def urls_post():
             flash('Страница уже существует', 'info')
         return redirect(url_for('url_get', id=site_id))
     else:
-        flash('Некорректный url', 'error')
+        flash('Некорректный url', 'danger')
         return redirect('/')
 
 
@@ -58,7 +59,8 @@ def url_get(id):
     cur = conn.cursor()
     cur.execute('SELECT * FROM urls WHERE id = (%s)', (id,))
     site_id, site_name, site_created_at = cur.fetchone()
-    cur.execute('SELECT id, created_at FROM url_checks WHERE url_id = (%s)',
+    cur.execute('SELECT id, status_code, created_at '
+                'FROM url_checks WHERE url_id = (%s)',
                 (id,))
     data = cur.fetchall()
     if data:
@@ -80,9 +82,11 @@ def urls_get():
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute('SELECT urls.id AS url_id, urls.name AS url_name, '
-                'MAX(url_checks.created_at) AS check_created_at FROM urls '
+                'MAX(url_checks.created_at) AS check_created_at,'
+                'url_checks.status_code AS status_code FROM urls '
                 'LEFT JOIN url_checks ON urls.id = url_checks.url_id '
-                'GROUP BY urls.id ORDER BY urls.created_at DESC')
+                'GROUP BY urls.id, url_checks.status_code '
+                'ORDER BY urls.created_at DESC')
     sites = cur.fetchall()
     cur.close()
     conn.close()
@@ -98,9 +102,18 @@ def url_check(id):
     cur = conn.cursor()
     today = datetime.datetime.now()
     created_at = datetime.date(today.year, today.month, today.day)
-    cur.execute('INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s)',
-                (id, created_at))
-    conn.commit()
+    cur.execute('SELECT name FROM urls WHERE id = (%s)', (id,))
+    url = cur.fetchone()[0]
+    try:
+        r = requests.get(url)
+        code = r.status_code
+        cur.execute('INSERT INTO url_checks (url_id, created_at, status_code) '
+                    'VALUES (%s, %s, %s)',
+                    (id, created_at, code))
+        conn.commit()
+    except requests.exceptions.ConnectionError:
+        flash('Произошла ошибка при проверке', 'danger')
+
     cur.close()
     conn.close()
     return redirect(url_for('url_get', id=id))
