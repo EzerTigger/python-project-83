8487de27f0059f7ba0f5791638a2bb4edf5f5e8f
Sergei Ezerskii <gaftezer@gmail.com>
2023-09-20 11:11:09 +0300

    Move parser, urls, requests to separate modules

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,12 +3,12 @@ from flask import redirect, url_for, abort
 import psycopg2
 import os
 import datetime
-import validators
-import requests
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 from psycopg2.extras import NamedTupleCursor
+
+from page_analyzer.urls import normalize_url, validate
+from page_analyzer.parser import page_parser
+from page_analyzer.requests import get_response
 
 
 load_dotenv()
@@ -17,44 +17,9 @@ app.secret_key = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 
-def normalize_url(url):
-    o = urlparse(url)
-    scheme = o.scheme
-    name = o.netloc
-    return f'{scheme}://{name}'
-
-
-def validate(start_url):
-    errors = []
-    if len(start_url) > 255:
-        errors.append('URL превышает 255 символов')
-    if not validators.url(start_url):
-        errors.append('Некорректный URL')
-    if not start_url:
-        errors.append('URL обязателен')
-    return errors
-
-
 def connect_db():
     conn = psycopg2.connect(app.config['DATABASE_URL'])
     return conn
-
-
-def page_parser(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    title: str = soup.find('title').text if soup.find('title') else ''
-    h1: str = soup.find('h1').text if soup.find('h1') else ''
-    description: str = ''
-    description_meta = soup.find('meta', attrs={'name': 'description'})
-
-    if description_meta:
-        description = description_meta['content']
-
-    return {
-        'title': title,
-        'h1': h1,
-        'description': description
-    }
 
 
 @app.route('/')
@@ -170,11 +135,8 @@ def url_check(id):
         )
         url = cursor.fetchone()
 
-    try:
-        response = requests.get(url.name)
-        response.raise_for_status()
-    except requests.exceptions.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
+    response = get_response(url.name)
+    if not response:
         return redirect(url_for('url_get', id=id))
 
     page_content = response.text
